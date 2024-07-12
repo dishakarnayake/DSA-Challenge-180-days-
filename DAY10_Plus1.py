0826ee070bafff2e292504447d9ba09ebea0eb77
dishakarnayake <dishakarnayake0805@gmail.com>
2024-07-12 20:26:25 +0530

    Day - 10
    Plus one

---
+++ DAY10_Plus1.py
@@ -0,0 +1,24 @@
+
+# using list comprehension
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
+    
+    
+# usinf iterative method [Brute force method]
+class solution(object):
+    def plusOne(self, digits):
+        
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            digits[i] += 1
+            digits[i] %= 10
+            if digits[i] != 0:
+                return digits
+        return [1] + digits
