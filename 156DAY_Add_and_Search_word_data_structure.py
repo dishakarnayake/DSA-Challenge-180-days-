a1f9ae408b7ed596a56db2ea5cecce685cc410e6
dishakarnayake <dishakarnayake0805@gmail.com>
2024-12-05 19:20:41 +0530

    Day - 156
    Design Add and Search Word Data Structure

---
+++ 156DAY_Add_and_Search_word_data_structure.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        def dfs(index, node):
+            if index == len(word):
+                return node.is_end_of_word
+            
+            char = word[index]
+            if char == '.':
+                for child in node.children.values():
+                    if dfs(index + 1, child):
+                        return True
+                return False
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(index + 1, node.children[char])
+        
+        return dfs(0, self.root)
+
+
+
+
+
+
